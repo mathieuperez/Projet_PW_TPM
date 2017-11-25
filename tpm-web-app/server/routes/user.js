c443230bb43c4fcb8ea166e7d36b3bc72ef318a7
catiaa <taoufik.zakia@gmail.Com>
2017-11-25 22:53:20 +0100

    model et route pour l'utilisateur

---
+++ tpm-web-app/server/routes/user.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../model/user.js');
+var userService = require('../services/user');
+
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+
+
+function authenticate(req, res) {
+    userService.authenticate(req.body.email, req.body.password)
+        .then(function (user) {
+            if (user) {
+                // authentication successful
+                res.send(user);
+            } else {
+                // authentication failed
+                res.status(400).send('Username or password is incorrect');
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function register(req, res) {
+    userService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+
+router.get('/', function(req, res, next) {
+  User.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+/* GET SINGLE USER BY ID */
+router.get('/:id', function(req, res, next) {
+  User.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* SAVE USER */
+router.post('/', function(req, res, next) {
+  User.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* UPDATE USER */
+router.put('/:id', function(req, res, next) {
+  User.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE USER */
+router.delete('/:id', function(req, res, next) {
+  User.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
