c969c17e0a0481811f59589a2b05265422c616eb
catiaa <taoufik.zakia@gmail.Com>
2017-12-07 13:33:25 +0100

    users avec 3 tableau:trajet/location/voyage

--- tpm-web-app/server/routes/users.js
+++ tpm-web-app/server/routes/users.js
@@ -8,8 +8,23 @@ var app = express();
 
 app.set('superSecret', "12345"); // secret variable
 
+mongoose.connect('mongodb://localhost:27017/tpm-webdb', { useMongoClient: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erreur lors de la connexion'));
+db.once('open', function (){
+    console.log("Connexion à la base OK");
+});
+
 var usersSchema = mongoose.Schema({
-    email:{
+    login:{
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        required: true
+  },
+  email:{
         type: String,
         unique: true,
         lowercase: true,
@@ -24,80 +39,61 @@ var usersSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    token: {
-        type: String,
-        required: false
-    },
-    offre: [String]
+   
+    trajet:[String],
+    location:[String],
+    voyage:[String]
+    
+  
 });
 
 var User = mongoose.model('User', usersSchema);
 
 router.post('/', (req, res) => {
     var user = new User();
-    user.email = req.body.email;
+    user.login = req.body.login;
     user.password = req.body.password;
     user.role = req.body.role;
 
-    if (user.email == null || user.password == null) {
-        res.status(422).json({success: false, message:'Missing Arguments.'});
-    }
-    else {
-        User.find().where('email').equals(user.email).exec(function(err, users){
-            if (err) {
-                res.status(500).json({success: false, message:'There was a problem with the database while checking if the email already exists.'});
-            }
-            else {
-                if(users.length==0){
-                    user.password = crypto.createHmac('sha256', user.password)
-                                   .update('I love cupcakes')
-                                   .digest('hex');
-                    user.save(function(err){
-                      if(err){
-                        res.status(401).json({success: false, message: 'Register failed.'});
-                      }
-                      else{
-                        res.status(200).json({success: true, message:'Register successful'});
-                      }
-                    })  
-                }
-                else{
-                    res.status(409).json({success: false, message: 'There is already a user with this email.'});
-                }
-            }
-        })
-    }
+
+    user.password = crypto.createHmac('sha256', user.password)
+                   .update('I love cupcakes')
+                   .digest('hex');
+
+    user.save(function(err){
+      if(err){
+        res.status(400).json({success: false, message: 'Register failed.'});
+      }
+    res.json({
+        success: true,
+        message : 'Register completed!'});
+    })
+
 });
 
 router.post('/token', (req, res) => {
     var user = new User();
     res.setHeader('Content-Type', 'application/json');
-    user.email = req.body.login;
+    user.login = req.body.login;
     user.password = req.body.password;
 
-    if (user.email == null || user.password == null) {
+    if (user.login == null || user.password == null) {
         res.status(422).json({success: false, message: 'Missing arguments.'});
     }
     else {
         user.password = crypto.createHmac('sha256', user.password)
                     .update('I love cupcakes')
                     .digest('hex');
-        User.findOne( {'email': user.email, 'password': user.password }, (error, users) => {
+        User.findOne( {'login': user.login, 'password': user.password } , (error, users ) => {
             if (users) {
                 var token = jwt.sign(users.toJSON(), app.get('superSecret'));
-                User.updateOne({ 'email': users.email }, { 'token': token }, (error,response) => {
-                    if (response['ok'] === 1) {
-                        res.json({
-                            success: true,
-                            message: 'Authentication succeded!',
-                            user: users
-                        });
-                    }
-                    else {
-                        res.status(400).json({success: false, message: 'Authentication failed.'});
-                    }
+                //
+                res.json({
+                    success: true,
+                    message: 'Authentication succeded!',
+                    token: token,
+                    user: users
                 });
-
             }
             else {
                 res.status(400).json({success: false, message: 'Authentication failed. Wrong login/password.'});
